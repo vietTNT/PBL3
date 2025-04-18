ef5abb1298dfd6a65f0dc203cb7f6db1d0d11d0f
Trần Đắc Đại Việt <102798747+vietTNT@users.noreply.github.com>
2025-04-18 19:53:49 +0700

    Delete PBL3 directory

--- PBL3/src/com/pbl/event/EventMenu.java
+++
@@ -1,8 +0,0 @@
-package com.pbl.event;
-
-import java.awt.Component;
-
-public interface EventMenu {
-
-    public boolean menuPressed(Component com, boolean open);
-}
--- PBL3/src/com/pbl/event/EventShowPopupMenu.java
+++
@@ -1,8 +0,0 @@
-package com.pbl.event;
-
-import java.awt.Component;
-
-public interface EventShowPopupMenu {
-
-    public void showPopup(Component com);
-}
